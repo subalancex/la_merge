0ca30a5d0541d4a7242ed4889750c8d8c3ea652b
subalancex <subalancex@gmail.com>
2018-12-27 18:04:36 +0200

    show time ; sleep for 60 seconds ; show time

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+		
+	      #################################################
+ 	      # Created by: Alex Zvigun
+	      # Date: Thu Dec 27 17:47:32 IST 2018
+	      # Purpose: Homework (Git Practicing)
+	      # Version: 0.0.1                                 
+	      #################################################
+
+date +%T ; sleep 60 ; date +%T	      
